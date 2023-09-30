0795dc8e0b828eeb7acf3359ed1e0a1fb29bf922
Spiffical <spencer.bialek@gmail.com>
2023-09-29 17:11:42 -0700

    added functionality for using multiple hdf5 files

--- mmpretrain/datasets/__init__.py
+++ mmpretrain/datasets/__init__.py
@@ -24,13 +24,14 @@ from .stanfordcars import StanfordCars
 from .sun397 import SUN397
 from .transforms import *  # noqa: F401,F403
 from .voc import VOC
+from .multihdf5dataset import MultiHDF5Dataset
 
 __all__ = [
     'BaseDataset', 'CIFAR10', 'CIFAR100', 'CUB', 'Caltech101', 'CustomDataset',
     'DTD', 'FGVCAircraft', 'FashionMNIST', 'Flowers102', 'Food101', 'ImageNet',
     'ImageNet21k', 'InShop', 'KFoldDataset', 'MNIST', 'MultiLabelDataset',
     'MultiTaskDataset', 'NLVR2', 'OxfordIIITPet', 'Places205', 'SUN397',
-    'StanfordCars', 'VOC', 'build_dataset'
+    'StanfordCars', 'VOC', 'build_dataset', 'MultiHDF5Dataset'
 ]
 
 if WITH_MULTIMODAL:
---
+++ mmpretrain/datasets/multihdf5dataset.py
@@ -0,0 +1,37 @@
+import os
+import h5py
+import numpy as np
+from mmpretrain.registry import DATASETS
+from mmengine.dataset import BaseDataset
+
+
+@DATASETS.register_module()
+class MultiHDF5Dataset(BaseDataset):
+    def __init__(self, data_root, samples_per_file=10000, test_mode=False, pipeline=[]):
+        self.data_root = data_root
+        self.hdf5_files = [os.path.join(data_root, f) for f in os.listdir(data_root) if f.endswith('.h5')]
+        self.samples_per_file = samples_per_file
+        super().__init__(test_mode=test_mode, lazy_init=True, pipeline=pipeline)
+
+    def full_init(self):
+        # Your custom initialization logic here
+        self._fully_initialized = True
+
+    def __len__(self):
+        return len(self.hdf5_files) * self.samples_per_file
+
+    def __getitem__(self, idx):
+        file_idx = idx // self.samples_per_file
+        sample_idx = idx % self.samples_per_file
+        with h5py.File(self.hdf5_files[file_idx], 'r') as f:
+            image = np.array(f['images'][sample_idx])
+
+        image = image.transpose((1, 2, 0))  # Change order to (H, W, channel)
+
+        # Prepare the data in the format expected by the pipeline
+        data = dict(img=image, supp_data=[])
+
+        # Process the data through the pipeline
+        data = self.pipeline(data)
+
+        return data
